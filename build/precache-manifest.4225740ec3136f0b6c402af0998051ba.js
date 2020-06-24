c7539ad5eb1239dbc5d38f27bedb62c53062e588
youone04 <yudi.gunaone87@gmail.com>
2020-06-24 18:03:58 +0700

    upload template

---
+++ build/precache-manifest.4225740ec3136f0b6c402af0998051ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921cf750279d02ddd2a79ee84648a6f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "112d759e609ef899407d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c27a25ced4d453da18",
+    "url": "/static/js/2.987928db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.987928db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112d759e609ef899407d",
+    "url": "/static/js/main.1c6756a2.chunk.js"
+  },
+  {
+    "revision": "e2616b225028fb07835a",
+    "url": "/static/js/runtime-main.1e8043c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
