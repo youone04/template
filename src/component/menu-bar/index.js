21351a48d128e82a6a2bf28b47bc0e905ce4818b
youone04 <yudi.gunaone87@gmail.com>
2020-06-24 18:54:46 +0700

    tambah navbar

---
+++ src/component/menu-bar/index.js
@@ -0,0 +1,52 @@
+import React, {Component,Fragment} from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
+import './menuBar.css';
+// import gambarPortopolio from "../../assets/gambar1.png";
+class MenuNavbar extends Component {
+
+    scrollTop = () => {
+        scroll.scrollTop();
+    };
+    render(){
+        return(
+            <Fragment>
+                <div className="navigation">
+                        <div className="gambar-portopolio">
+                            {/* <img src={gambarPortopolio} alt="gambar portopolio" /> */}
+                        </div>
+                        <p><b>Yudi Gunawan</b></p>
+                    <div className="navbar">
+                        <Link
+                            activeClass="active"
+                            to="kontak"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >kontak</Link>
+
+                        <Link
+                            activeClass="active"
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >About</Link>
+
+                        <Link
+                            activeClass="active"
+                            to="halamanutama"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >Home</Link>
+
+                    </div>
+                </div>
+                </Fragment>
+        )
+    }
+}
+export default MenuNavbar;
